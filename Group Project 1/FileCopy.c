413dc7b6687e51d2985578e33f404435eb37a67a
Fozhan Babaeiyan <babaeeianwork@gmail.com>
2024-09-23 12:47:02 -0700

    files
    I added the files for the coding assignment

---
+++ Group Project 1/FileCopy.c
@@ -0,0 +1,18 @@
+/*
+ * FileCopy.c
+ * 
+ * This program opens a file and writes its contents to a pipe 
+ */
+ 
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+#define BUFFER_SIZE 25
+#define STRING_SIZE 25
+
+int main(int argc, char *argv[])
+{
+   // Your code is here
+               
+}
---
+++ Group Project 1/child.c
@@ -0,0 +1,15 @@
+/*
+ * child.c
+ *
+ * This program reads the contents of the pipe and writes it to a file.
+ */
+
+#include <stdio.h>
+#include <windows.h>
+
+#define BUFFER_SIZE 10
+
+int main(int argc, char *argv[])
+{
+    // Your code is here
+}
